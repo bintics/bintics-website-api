d37649b777faed0fd8053f48165361d0572f7ff1
Federico Farias Sánchez <federico.farias@optimissa.com.mx>
2017-02-08 13:22:56 -0600

    Fix de secciones de navegación

--- web-app/app/Providers/AppServiceProvider.php
+++ web-app/app/Providers/AppServiceProvider.php
@@ -14,17 +14,23 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        $nav_top_items = Section::where('name', 'NavTop')->first();
-        view()->share('nav_top_items', $nav_top_items->sections);
+        $nav_top_items = $this->getNavItems('NavTop');
+        view()->share('nav_top_items', $nav_top_items);
 
-        $nav_left_items = Section::where('name', 'NavLeft')->first();
-        view()->share('nav_left_items', $nav_left_items->sections);
+        $nav_left_items = $this->getNavItems('NavLeft');
+        view()->share('nav_left_items', $nav_left_items);
 
-        $nav_right_items = Section::where('name', 'NavRight')->first();
-        view()->share('nav_right_items', $nav_right_items->sections);
+        $nav_right_items = $this->getNavItems('NavRight');
+        view()->share('nav_right_items', $nav_right_items);
 
-        $nav_bottom_items = Section::where('name', 'NavBottom')->first();
-        view()->share('nav_bottom_items', $nav_bottom_items->sections);
+        $nav_bottom_items = $this->getNavItems('NavBottom');
+        view()->share('nav_bottom_items', $nav_bottom_items);
+    }
+
+    private function getNavItems($name) {
+        $nav = Section::where('name', $name)->first();
+        $nav_items = is_null($nav) || is_null($nav->sections) ? [] : $nav->sections;
+        return $nav_items;
     }
 
     /**
